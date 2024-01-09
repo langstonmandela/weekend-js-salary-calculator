bc5f42f6190d1ce5d9cdda4b85885b8b83527118
Langston Johnson <langstonjohnson@MacBook-Pro.local>
2024-01-09 17:27:04 -0600

    update form inputs to be required, and changed ID input type to text

--- bonus-calc.js
+++ bonus-calc.js
@@ -1,1 +1,28 @@
-console.log('---- Weekend Bonus Calcutor ----');
+console.log("---- Weekend Bonus Calcutor ----");
+
+
+function onReady(){
+    console.log('DOM is Ready');
+
+    const form = document.getElementById("employeeForm");
+    const firstNameInput = document.getElementById("firstName");
+    
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const inputs = document.querySelectorAll("#employeeForm input");
+
+    inputs.forEach((input) => {
+        console.log(`${input.id}: ${input.value}`);
+    });
+});
+
+onReady();
