ede7ea9beb15106b302755f54f672df239ae40bf
Langston Johnson <langstonjohnson@MacBook-Pro.local>
2024-01-09 19:14:08 -0600

    delete function wired and logged

--- bonus-calc.js
+++ bonus-calc.js
@@ -1,18 +1,11 @@
 console.log("---- Weekend Bonus Calcutor ----");
 
 
-function onReady(){
-    console.log('DOM is Ready');
-
-    const form = document.getElementById("employeeForm");
-    const firstNameInput = document.getElementById("firstName");
-    
-}
 
 function submitForm(event){
     event.preventDefault();
     console.log('Submitted Form');
-
+    
     const firstName = document.getElementById("firstName").value;
     const lastName = document.getElementById("lastName").value;
     const employeeID = document.getElementById("employeeID").value;
@@ -21,9 +14,9 @@ function submitForm(event){
     console.log(firstName, lastName, employeeID, jobTitle, annualSalary );
     
     const tableBody = document.querySelector("tbody");
-
+    
     const newRow = document.createElement("tr");
-
+    
     newRow.innerHTML = `
         <td>${firstName}</td>
         <td>${lastName}</td>
@@ -31,18 +24,32 @@ function submitForm(event){
         <td>${jobTitle}</td>
         <td>${annualSalary}</td>
         <td><button class="delete-button" onClick="removeRow(event)">Delete</button></td>`; //remove row function will need to be built
+        
+        
+        tableBody.appendChild(newRow);
+        
+        console.log(tableBody);
+        
+        document.getElementById("firstName").value = '';
+        document.getElementById("lastName").value = '';
+        document.getElementById("employeeID").value = '';
+        document.getElementById("jobTitle").value = '';
+        document.getElementById("annualSalary").value = '';
+        
+    }
+
+    function removeRow(event){
+        console.log(event.target);
+        event.target.closest('tr').remove();
+        
+    }
 
 
-    tableBody.appendChild(newRow);
 
-    console.log(tableBody);
 
-    document.getElementById("firstName").value = '';
-    document.getElementById("lastName").value = '';
-    document.getElementById("employeeID").value = '';
-    document.getElementById("jobTitle").value = '';
-    document.getElementById("annualSalary").value = '';
+
+    function onReady(){
+        console.log('DOM is Ready');  
+    }
     
-}
-
-onReady();
+    onReady();
